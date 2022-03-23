ee049dd6665d9b7a1f76103acce8b635fc5dd9ca
fargelus <ddraudred@gmail.com>
2022-03-23 16:41:35 +0300

    Fixed messages broadcasting

--- app/channels/robots/beep_channel.rb
+++ app/channels/robots/beep_channel.rb
@@ -5,8 +5,8 @@ module Robots
     end
 
     def speak(data)
-      ActionCable.server.broadcast "robots", message: data["text"], uid: uid
-      
+      ActionCable.server.broadcast "robots", { message: data["text"], uid: uid }
+
       probably_respond
     end
 
@@ -15,11 +15,11 @@ module Robots
     def probably_respond
       return unless rand > 0.4
 
-      ActionCable.server.broadcast "robots", message: some_message, uid: "robot"
+      ActionCable.server.broadcast "robots", { message: some_message, uid: "robot" }
     end
 
     def some_message
-      Faker::StarWars.public_send(%i[wookiee_sentence quote].sample)
+      Faker::Movies::StarWars.public_send(%i[wookiee_sentence quote].sample)
     end
   end
 end
--- engines/martians/app/channels/martians/human_channel.rb
+++ engines/martians/app/channels/martians/human_channel.rb
@@ -5,9 +5,9 @@ module Martians
     end
 
     def speak(data)
-      Martians::Engine.server.broadcast "robots", message: data["text"], uid: uid
+      Martians::Engine.server.broadcast "robots", { message: data["text"], uid: uid }
 
-      Martians::Engine.server.broadcast "robots", message: "Resistence is futile!", uid: "martian"
+      Martians::Engine.server.broadcast "robots", { message: "Resistence is futile!", uid: "martian" }
     end
   end
 end
